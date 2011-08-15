719e8e2743fb3a858b19557dbd1751ae430d968b
Jeremy Keeshin <jkeesh@gmail.com>
2011-08-14 19:26:01 -0700

    diffs show ids

--- viz/templatetags/data_tags.py
+++ viz/templatetags/data_tags.py
@@ -2,14 +2,19 @@ from django import template
 register = template.Library()
 from viz.models import DataPoint
 
+from viz.data_points import diff_data_points
+
 @register.inclusion_tag('data_diff.html')
 def previous_diff(data_point):
-    print data_point
     prev = DataPoint.get_previous_by_created_at(data_point)
-    print prev 
+    lost, gained = diff_data_points(prev, data_point)
+    
+    
     return {
         'data_point': data_point,
-        'prev': prev
+        'prev': prev,
+        'lost': lost,
+        'gained': gained
     }
     
     
