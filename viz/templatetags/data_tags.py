554913b612d81abd22895626fc98daa2947621d7
Jeremy Keeshin <jkeesh@gmail.com>
2011-08-15 14:26:42 -0700

    fix select two login

--- viz/templatetags/data_tags.py
+++ viz/templatetags/data_tags.py
@@ -6,7 +6,7 @@ from viz.data import diff_data_points
 
 @register.inclusion_tag('data_diff.html')
 def previous_diff(data_point):
-    prev = DataPoint.get_previous_by_created_at(data_point)
+    prev = DataPoint.get_previous_by_created_at(data_point, user=data_point.user)
     lost, gained = diff_data_points(prev, data_point)
     
     
